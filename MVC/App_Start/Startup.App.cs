1201c4904cd662ab7728372ec0c7c89cca11669a
dkjaer <dkjaer@hotmail.com>
2017-11-13 16:31:13 -0600

    Added the Microsoft RESTful API project code.  Will be adding code from an actual project next.

---
+++ MVC/App_Start/Startup.App.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+//using Mvc.Models;
+
+namespace Mvc
+{
+    public partial class Startup
+    {
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="app"></param>
+		public void ConfigureAuth(IAppBuilder app)
+        {
+
+        }
+    }
+}
---
+++ RestfulApi/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace RestfulApi.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Title = "Code Samples";
+
+			return View();
+		}
+	}
+}
